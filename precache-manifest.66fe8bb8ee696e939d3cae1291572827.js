48b997895bc73d26895a690ebeeebc97dfe523ad
Aditya Pandey <adityadpandey@gmail.com>
2020-06-24 11:48:11 +0530

    Updates

---
+++ precache-manifest.66fe8bb8ee696e939d3cae1291572827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fab699ca1419bee99d181b4b2265fa",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "bcc58d6f1474cace31a3",
+    "url": "/react-calculator/static/css/main.8f89222c.chunk.css"
+  },
+  {
+    "revision": "5ef5d9d76ca743102781",
+    "url": "/react-calculator/static/js/2.6790f964.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.6790f964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc58d6f1474cace31a3",
+    "url": "/react-calculator/static/js/main.2fdd911c.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
